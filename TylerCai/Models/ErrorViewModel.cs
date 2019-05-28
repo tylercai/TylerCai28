c87eb3a8c48d5254e73139f433574205193dfdb7
tylercai28 <tylercai28@gmail.com>
2019-05-28 15:01:17 -0400

    * .gitignore: Deploy basic MVC Project
    * TylerCai.sln:
    * Startup.cs:
    * Program.cs:
    * TylerCai.csproj:
    * appsettings.json:
    * site.js:
    * favicon.ico:
    * site.css:
    * site.min.js:
    * _ViewStart.cshtml:
    * About.cshtml:
    * Index.cshtml:
    * site.min.css:
    * ErrorViewModel.cs:
    * Contact.cshtml:
    * Error.cshtml:
    * Privacy.cshtml:
    * _ViewImports.cshtml:
    * banner3.svg:
    * banner2.svg:
    * banner1.svg:
    * _Layout.cshtml:
    * appsettings.Development.json:
    * LICENSE:
    * HomeController.cs:
    * .bower.json:
    * LICENSE.txt:
    * launchSettings.json:
    * jquery.js:
    * .bower.json:
    * npm.js:
    * jquery.min.js:
    * jquery.min.map:
    * LICENSE.md:
    * _CookieConsentPartial.cshtml:
    * .bower.json:
    * bootstrap.js:
    * bootstrap.css:
    * _ValidationScriptsPartial.cshtml:
    * bootstrap.min.js:
    * bootstrap.css.map:
    * bootstrap.min.css:
    * bootstrap-theme.css:
    * bootstrap.min.css.map:
    * .bower.json:
    * jquery.validate.js:
    * LICENSE.txt:
    * bootstrap-theme.css.map:
    * bootstrap-theme.min.css:
    * additional-methods.js:
    * jquery.validate.min.js:
    * bootstrap-theme.min.css.map:
    * additional-methods.min.js:
    * glyphicons-halflings-regular.ttf:
    * glyphicons-halflings-regular.svg:
    * glyphicons-halflings-regular.eot:
    * glyphicons-halflings-regular.woff:
    * glyphicons-halflings-regular.woff2:
    * jquery.validate.unobtrusive.js:
    * jquery.validate.unobtrusive.min.js:

---
+++ TylerCai/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TylerCai.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
